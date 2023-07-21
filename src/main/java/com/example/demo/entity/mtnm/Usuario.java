68ab9b6166508e6023dab41fc0ad5f21781884cf
Dennis040119 <HTML@ipartekaula.com>
2023-07-21 11:35:43 +0200

    ultimo 21-07-23

--- src/main/java/com/example/demo/entity/mtnm/Usuario.java
+++ src/main/java/com/example/demo/entity/mtnm/Usuario.java
@@ -1,16 +1,20 @@
 package com.example.demo.entity.mtnm;
 
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.springframework.stereotype.Component;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-import lombok.Data;
-
-@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
 @Table(name = "usuario")
 public class Usuario {
@@ -24,98 +28,8 @@ public class Usuario {
 	private String direccion;
 	private String estado;
 	private String rol;
+	private String imagen;
 	
-	
-	public Usuario(String userid, String username, String password, String email, String tarjetaCredito, String direccion,
-			String estado, String rol) {
-		super();
-		this.userid = userid;
-		this.username = username;
-		this.password = password;
-		this.email = email;
-		this.tarjetaCredito = tarjetaCredito;
-		this.direccion = direccion;
-		this.estado = estado;
-		this.rol = rol;
-	}
-	
-	
-	public Usuario() {
-		super();
-	}
-
-
-	public String getUserid() {
-		return userid;
-	}
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
-
-	
-	
-	public String getUsername() {
-		return username;
-	}
-
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getTarjetaCredito() {
-		return tarjetaCredito;
-	}
-	public void setTarjetaCredito(String tarjetaCredito) {
-		this.tarjetaCredito = tarjetaCredito;
-	}
-	public String getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-	public String getRol() {
-		return rol;
-	}
-	public void setRol(String rol) {
-		this.rol = rol;
-	}
-	
-	
-	
-	
-	
-	public String getEstado() {
-		return estado;
-	}
-
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-
-
-	@Override
-	public String toString() {
-		return "Usuario [userid=" + userid + ", username=" + username + ", password=" + password + ", email=" + email
-				+ ", tarjetaCredito=" + tarjetaCredito + ", direccion=" + direccion + ", estado=" + estado + ", rol="
-				+ rol + "]";
-	}
-
 
 	public  static String  generarcodigo(int nro) {
 		if(nro<9) {return "us00"+(nro+1);}
